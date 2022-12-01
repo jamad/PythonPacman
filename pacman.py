0ceabe9fc247ef4b97891873cafe7a09c2b04bb0
jamad <jamad2001@gmail.com>
2022-12-02 00:39:43 +0200

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -8,6 +8,11 @@ pygame.init()
 
 WIDTH = 900
 HEIGHT = 950
+
+num1 = (HEIGHT - 50) // 32
+num2 = (WIDTH // 30)
+num3 = 15 # what's this?
+
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
@@ -98,9 +103,8 @@ class Ghost:
 
     def check_collisions(self):
         # R, L, U, D
-        num1 = ((HEIGHT - 50) // 32)
-        num2 = (WIDTH // 30)
-        num3 = 15
+        
+        num3 = 15  # what's this? 
         self.turns = [False, False, False, False]
         if 0 < self.center_x // 30 < 29:
             if level[(self.center_y - num3) // num1][self.center_x // num2] == 9:                self.turns[2] = True
@@ -122,23 +126,17 @@ class Ghost:
                     if level[(self.center_y + num3) // num1][self.center_x // num2] < 3 or (level[(self.center_y + num3) // num1][self.center_x // num2] == 9 and (self.in_box or self.dead)):self.turns[3] = True
                     if level[(self.center_y - num3) // num1][self.center_x // num2] < 3 or (level[(self.center_y - num3) // num1][self.center_x // num2] == 9 and (self.in_box or self.dead)):self.turns[2] = True
                 if 12 <= self.center_y % num1 <= 18:
-                    if level[self.center_y // num1][(self.center_x - num3) // num2] < 3 or (level[self.center_y // num1][(self.center_x - num3) // num2] == 9 and (self.in_box or self.dead)):                        self.turns[1] = True
-                    if level[self.center_y // num1][(self.center_x + num3) // num2] < 3 or (level[self.center_y // num1][(self.center_x + num3) // num2] == 9 and (self.in_box or self.dead)):                        self.turns[0] = True
-        else:
-            self.turns[0] = True
-            self.turns[1] = True
-        if 350 < self.x_pos < 550 and 370 < self.y_pos < 480:
-            self.in_box = True
-        else:
-            self.in_box = False
+                    if level[self.center_y // num1][(self.center_x - num3) // num2] < 3 or (level[self.center_y // num1][(self.center_x - num3) // num2] == 9 and (self.in_box or self.dead)):self.turns[1] = True
+                    if level[self.center_y // num1][(self.center_x + num3) // num2] < 3 or (level[self.center_y // num1][(self.center_x + num3) // num2] == 9 and (self.in_box or self.dead)):self.turns[0] = True
+        else: self.turns[0] = self.turns[1] = 1
+        self.in_box = (350 < self.x_pos < 550 and 370 < self.y_pos < 480)
         return self.turns, self.in_box
 
     def move_clyde(self):
         # r, l, u, d
         # clyde is going to turn whenever advantageous for pursuit
         if self.direction == 0:
-            if self.target[0] > self.x_pos and self.turns[0]:
-                self.x_pos += self.speed
+            if self.target[0] > self.x_pos and self.turns[0]:                self.x_pos += self.speed
             elif not self.turns[0]:
                 if self.target[1] > self.y_pos and self.turns[3]:
                     self.direction = 3
@@ -165,13 +163,10 @@ class Ghost:
                 if self.target[1] < self.y_pos and self.turns[2]:
                     self.direction = 2
                     self.y_pos -= self.speed
-                else:
-                    self.x_pos += self.speed
+                else:                    self.x_pos += self.speed
         elif self.direction == 1:
-            if self.target[1] > self.y_pos and self.turns[3]:
-                self.direction = 3
-            elif self.target[0] < self.x_pos and self.turns[1]:
-                self.x_pos -= self.speed
+            if self.target[1] > self.y_pos and self.turns[3]:                self.direction = 3
+            elif self.target[0] < self.x_pos and self.turns[1]:                self.x_pos -= self.speed
             elif not self.turns[1]:
                 if self.target[1] > self.y_pos and self.turns[3]:
                     self.direction = 3
@@ -198,8 +193,7 @@ class Ghost:
                 if self.target[1] < self.y_pos and self.turns[2]:
                     self.direction = 2
                     self.y_pos -= self.speed
-                else:
-                    self.x_pos -= self.speed
+                else:                    self.x_pos -= self.speed
         elif self.direction == 2:
             if self.target[0] < self.x_pos and self.turns[1]:
                 self.direction = 1
@@ -264,12 +258,8 @@ class Ghost:
                 elif self.target[0] < self.x_pos and self.turns[1]:
                     self.direction = 1
                     self.x_pos -= self.speed
-                else:
-                    self.y_pos += self.speed
-        if self.x_pos < -30:
-            self.x_pos = 900
-        elif self.x_pos > 900:
-            self.x_pos - 30
+                else:self.y_pos += self.speed
+        self.x_pos = (self.x_pos < -30 and 900) or ( self.x_pos > 900 and self.x_pos - 30) or self.x_pos            
         return self.x_pos, self.y_pos, self.direction
 
     def move_blinky(self):
@@ -628,15 +618,17 @@ class Ghost:
 def draw_misc():
     score_text = font.render(f'Score: {score}', True, 'white')
     screen.blit(score_text, (10, 920))
-    if powerup:
-        pygame.draw.circle(screen, 'blue', (140, 930), 15)
-    for i in range(lives):
-        screen.blit(pygame.transform.scale(player_images[0], (30, 30)), (650 + i * 40, 915))
+    
+    if powerup:        pygame.draw.circle(screen, 'blue', (140, 930), 15)
+
+    for i in range(lives):        screen.blit(pygame.transform.scale(player_images[0], (30, 30)), (650 + i * 40, 915))
+    
     if game_over:
         pygame.draw.rect(screen, 'white', [50, 200, 800, 300],0, 10)
         pygame.draw.rect(screen, 'dark gray', [70, 220, 760, 260], 0, 10)
         gameover_text = font.render('Game over! Space bar to restart!', True, 'red')
         screen.blit(gameover_text, (100, 300))
+    
     if game_won:
         pygame.draw.rect(screen, 'white', [50, 200, 800, 300],0, 10)
         pygame.draw.rect(screen, 'dark gray', [70, 220, 760, 260], 0, 10)
@@ -644,23 +636,21 @@ def draw_misc():
         screen.blit(gameover_text, (100, 300))
 
 def check_collisions(scor, power, power_count, eaten_ghosts):
-    num1 = (HEIGHT - 50) // 32
-    num2 = WIDTH // 30
     if 0 < player_x < 870:
-        if level[center_y // num1][center_x // num2] == 1:
-            level[center_y // num1][center_x // num2] = 0
+        idx1=center_y // num1
+        idx2=center_x // num2
+        if level[idx1][idx2] == 1:
+            level[idx1][idx2] = 0
             scor += 10
-        if level[center_y // num1][center_x // num2] == 2:
-            level[center_y // num1][center_x // num2] = 0
+        if level[idx1][idx2] == 2:
+            level[idx1][idx2] = 0
             scor += 50
             power = True
             power_count = 0
-            eaten_ghosts = [False, False, False, False]
+            eaten_ghosts = [0]*4
     return scor, power, power_count, eaten_ghosts
 
 def draw_board():
-    num1 = ((HEIGHT - 50) // 32)
-    num2 = (WIDTH // 30)
     for i in range(len(level)):
         for j in range(len(level[i])):
             if level[i][j] == 1:                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
@@ -685,39 +675,28 @@ def draw_player():
         screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
 
 def check_position(centerx, centery):
-    turns = [False, False, False, False]
-    num1 = (HEIGHT - 50) // 32
-    num2 = (WIDTH // 30)
-    num3 = 15
+    turns = [0]*4
     # check collisions based on center x and center y of player +/- fudge number
     if centerx // 30 < 29:
-        if direction == 0:
-            if level[centery // num1][(centerx - num3) // num2] < 3:                turns[1] = True
-        if direction == 1:
-            if level[centery // num1][(centerx + num3) // num2] < 3:                turns[0] = True
-        if direction == 2:
-            if level[(centery + num3) // num1][centerx // num2] < 3:                turns[3] = True
-        if direction == 3:
-            if level[(centery - num3) // num1][centerx // num2] < 3:                turns[2] = True
-
-        if direction == 2 or direction == 3:
+        if direction == 0 and level[centery // num1][(centerx - num3) // num2] < 3:                turns[1] = True
+        if direction == 1 and level[centery // num1][(centerx + num3) // num2] < 3:                turns[0] = True
+        if direction == 2 and level[(centery + num3) // num1][centerx // num2] < 3:                turns[3] = True
+        if direction == 3 and level[(centery - num3) // num1][centerx // num2] < 3:                turns[2] = True
+        if direction in (2,3):
             if 12 <= centerx % num2 <= 18:
                 if level[(centery + num3) // num1][centerx // num2] < 3:                    turns[3] = True
                 if level[(centery - num3) // num1][centerx // num2] < 3:                    turns[2] = True
             if 12 <= centery % num1 <= 18:
                 if level[centery // num1][(centerx - num2) // num2] < 3:                    turns[1] = True
                 if level[centery // num1][(centerx + num2) // num2] < 3:                    turns[0] = True
-        if direction == 0 or direction == 1:
+        if direction in (0,1):
             if 12 <= centerx % num2 <= 18:
                 if level[(centery + num1) // num1][centerx // num2] < 3:                    turns[3] = True
                 if level[(centery - num1) // num1][centerx // num2] < 3:                    turns[2] = True
             if 12 <= centery % num1 <= 18:
                 if level[centery // num1][(centerx - num3) // num2] < 3:                    turns[1] = True
                 if level[centery // num1][(centerx + num3) // num2] < 3:                    turns[0] = True
-    else:
-        turns[0] = True
-        turns[1] = True
-
+    else: turns[0] = turns[1] = True
     return turns
 
 def move_player(play_x, play_y):
