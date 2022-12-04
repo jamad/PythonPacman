94759a6b305c044441e418f536971ab63e7bc2e8
jamad <jamad2001@gmail.com>
2022-12-04 12:21:06 +0200

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -88,12 +88,15 @@ class Ghost:
 
             not_alive= (self.in_box or self.dead)
 
-            self.turns[0] = (cellC < 3 or (cellC == 9 and not_alive)) or (self.dir in (2,3) and (12 <= self.center_y % COUNT_R <= 18)and (cellF < 3 or (cellF == 9 and not_alive)))
-            self.turns[1] = (cellB < 3 or (cellB == 9 and not_alive)) or (self.dir in (2,3) and (12 <= self.center_y % COUNT_R <= 18)and (cellE < 3 or (cellE == 9 and not_alive)))
-            self.turns[2] = (cellA < 3 or (cellA == 9 and not_alive)) or (self.dir in (2,3) and (12 <= self.center_x % COUNT_C <= 18)and (cellA < 3 or (cellA == 9 and not_alive))) or (cellA == 9)
-            self.turns[3] = (cellD < 3 or (cellD == 9 and not_alive)) or (self.dir in (2,3) and (12 <= self.center_x % COUNT_C <= 18)and (cellD < 3 or (cellD == 9 and not_alive)))
+            is_dirV=self.dir in (2,3)
+            is_dirH=self.dir in (0,1)
 
-            if self.dir in (0,1):
+            self.turns[0] = (cellC < 3 or (cellC == 9 and not_alive)) or (is_dirV and (12 <= self.center_y % COUNT_R <= 18)and (cellF < 3 or (cellF == 9 and not_alive)))
+            self.turns[1] = (cellB < 3 or (cellB == 9 and not_alive)) or (is_dirV and (12 <= self.center_y % COUNT_R <= 18)and (cellE < 3 or (cellE == 9 and not_alive)))
+            self.turns[2] = (cellA < 3 or (cellA == 9 and not_alive)) or (is_dirV and (12 <= self.center_x % COUNT_C <= 18)and (cellA < 3 or (cellA == 9 and not_alive))) or (cellA == 9)
+            self.turns[3] = (cellD < 3 or (cellD == 9 and not_alive)) or (is_dirV and (12 <= self.center_x % COUNT_C <= 18)and (cellD < 3 or (cellD == 9 and not_alive)))
+
+            if is_dirH:
                 if 12 <= self.center_y % COUNT_R <= 18:
                     if cellC < 3 or (cellC == 9 and not_alive):self.turns[0] = True
                     if cellB < 3 or (cellB == 9 and not_alive):self.turns[1] = True
