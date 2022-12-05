2f446f90e1ec62d6627d314ca8fbec8f4c0d21c3
jamad <jamad2001@gmail.com>
2022-12-05 17:53:25 +0200

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -209,17 +209,17 @@ class Ghost:
 
         if index==1:# GHOST[1] turns up or down at any point to pursue, but left and right only on collision
             if self.dir == 0:
-                if cond0:        pass
-                elif not self.can_move[0]:
-                    if cond3:    self.dir = 3
-                    elif cond2:  self.dir = 2
-                    elif cond1:  self.dir = 1
-                    elif self.can_move[3]:  self.dir = 3
-                    elif self.can_move[2]:  self.dir = 2
-                    elif self.can_move[1]:  self.dir = 1
-                elif self.can_move[0]:
-                    if cond3:    self.dir = 3
-                    if cond2:    self.dir = 2
+                if not cond0:
+                    if not self.can_move[0]:
+                        if cond3:    self.dir = 3
+                        elif cond2:  self.dir = 2
+                        elif cond1:  self.dir = 1
+                        elif self.can_move[3]:  self.dir = 3
+                        elif self.can_move[2]:  self.dir = 2
+                        elif self.can_move[1]:  self.dir = 1
+                    else:
+                        if cond3:    self.dir = 3
+                        if cond2:    self.dir = 2
             elif self.dir == 1:
                 if cond3:        self.dir = 3
                 elif cond1:      pass
@@ -254,8 +254,7 @@ class Ghost:
                     
         if index==2:# GHOST[2] is going to turn left or right whenever advantageous, but only up or down on collision
             if self.dir == 0:
-                if cond0:        pass
-                elif not self.can_move[0]:
+                if not cond0 and not self.can_move[0]:
                     if cond3:    self.dir = 3
                     elif cond2:  self.dir = 2
                     elif cond1:  self.dir = 1
@@ -264,7 +263,6 @@ class Ghost:
                     elif self.can_move[1]:  self.dir = 1
             elif self.dir == 1:
                 if cond3:                self.dir = 3
-                elif cond1:              pass
                 elif not self.can_move[1]:
                     if cond3:               self.dir = 3
                     elif cond2:             self.dir = 2
@@ -274,7 +272,6 @@ class Ghost:
                     elif self.can_move[0]:  self.dir = 0
             elif self.dir == 2:
                 if cond1:                    self.dir = 1
-                elif cond2:                    pass
                 elif not self.can_move[2]:
                     if cond0:               self.dir = 0
                     elif cond1:             self.dir = 1
@@ -309,8 +306,8 @@ class Ghost:
                         elif self.can_move[2]:  self.dir = 2
                         elif self.can_move[1]:  self.dir = 1
                     elif self.can_move[0]:
-                        if cond3:                        self.dir = 3
-                        if cond2:                        self.dir = 2
+                        if cond3:               self.dir = 3
+                        if cond2:               self.dir = 2
             elif self.dir == 1:
                 if cond3:   self.dir = 3
                 elif not self.can_move[1]:
