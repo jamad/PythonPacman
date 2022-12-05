97711e2804de6f16bb7a61115c7d6cfa6910641a
jamad <jamad2001@gmail.com>
2022-12-05 19:32:29 +0200

    fixed the red ghost behavior

--- pacman.py
+++ pacman.py
@@ -190,10 +190,12 @@ class Ghost:
         if index==0:  # GHOST[0] : clyde doesn't change direction unless hit . if multiple candidates to turn, follow pacman
             if any( (self.dir== i and not self.can_move[i]) for i in (0,1)): #blocked
                 if cond2:self.dir=2 # if can follow pacman above, go up
-                else:self.dir=3
-            if any( (self.dir== i and not self.can_move[i]) for i in (2,3) ):
+                elif cond3:self.dir=3
+                else:self.dir=(0,1)[self.dir==0] # backward
+            elif any( (self.dir== i and not self.can_move[i]) for i in (2,3) ):
                 if cond0:self.dir=0
-                else:self.dir=1
+                elif cond1:self.dir=1
+                else:self.dir=(2,3)[self.dir==2] # backward
 
         if index==1:# GHOST[1] turns up or down at any point to pursue, but left and right only on collision
             if self.dir == 0:
