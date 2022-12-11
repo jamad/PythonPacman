d3a44bdb8cd71213e81a5e273783bbfa385ddb01
jamad <jamad2001@gmail.com>
2022-12-11 22:58:33 +0200

    Update tutorial_before_pacman.py

--- tutorial_before_pacman.py
+++ tutorial_before_pacman.py
@@ -1,8 +1,7 @@
 # https://w3cschoool.com/tutorial/pygame-tutorial
 
 from pygame import init,sprite,Surface,time,display,K_LEFT, K_RIGHT, K_UP, K_DOWN, event,QUIT,key
-import sys
-
+#import sys
 
 class Sprite(sprite.Sprite):
     def __init__(self, pos):
@@ -12,53 +11,56 @@ class Sprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
-def main():
-    init()
-    clock = time.Clock()
-    fps = 50
-    bg = [0, 0, 0]
-    size =[300, 300]
-    screen = display.set_mode(size)
-    player = Sprite([40, 50])
-    # Define keys for player movement
-    player.move = [K_LEFT, K_RIGHT, K_UP, K_DOWN]
-    player.vx = 5
-    player.vy = 5
+fps = 50
+size =[300, 300]
 
-    wall = Sprite([100, 60])
+init()
 
-    wall_group = sprite.Group()
-    wall_group.add(wall)
+clock = time.Clock()
+screen = display.set_mode(size)
 
-    player_group = sprite.Group()
-    player_group.add(player)
+player = Sprite([40, 50])
+# Define keys for player movement
+player.move = [K_LEFT, K_RIGHT, K_UP, K_DOWN]
+player.vx = 5
+player.vy = 5
+player.image.fill((255, 255, 0)) 
 
-    while True:
-        for e in event.get():
-            if e.type == QUIT:
-                return False
-        k = key.get_pressed()
-        for i in range(2):
-            if k[player.move[i]]:
-                player.rect.x += player.vx * [-1, 1][i]
+wall = Sprite([100, 60])
 
-        for i in range(2):
-            if k[player.move[2:4][i]]:
-                player.rect.y += player.vy * [-1, 1][i]
-        screen.fill(bg)
-        # first parameter takes a single sprite
-        # second parameter takes sprite groups
-        # third parameter is a kill command if true
-        hit = sprite.spritecollide(player, wall_group, True)
-        if hit:
-        # if collision is detected call a function to destroy
-            # rect
-            player.image.fill((255, 255, 255))
-        player_group.draw(screen)
-        wall_group.draw(screen)
-        display.update()
-        clock.tick(fps)
-    quit()
-    sys.exit
+wall_group = sprite.Group()
+wall_group.add(wall)
 
-if __name__ == '__main__':main()
+player_group = sprite.Group()
+player_group.add(player)
+
+def mainloop():
+    if any(e.type == QUIT for e in event.get()):return 0
+    return 1
+
+def userinput():
+    k = key.get_pressed()
+    for i in range(2):
+        if k[player.move[i]]:       player.rect.x += player.vx * [-1, 1][i]
+
+    for i in range(2):
+        if k[player.move[2:4][i]]:  player.rect.y += player.vy * [-1, 1][i]
+
+def collisioncheck():
+    hit = sprite.spritecollide(player, wall_group, True)
+    if hit:
+        player.image.fill((255, 255, 255)) # if collision is detected call a function to destroy
+
+def drawscreen():
+    screen.fill([0, 0, 0])
+    player_group.draw(screen)
+    wall_group.draw(screen)
+    display.update()
+
+while mainloop():
+    userinput()
+    collisioncheck()
+    drawscreen()
+    clock.tick(fps)
+    
+print('all done')
