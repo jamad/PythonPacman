048a26e392c66f7f268338e62c754292e57fa8d1
jamad <jamad2001@gmail.com>
2023-01-05 14:57:29 +0200

    made the sprite visible

--- zelda_style_game_level.py
+++ zelda_style_game_level.py
@@ -1,12 +1,29 @@
 from pygame import *
+from zelda_style_game_settings import *
+from zelda_style_game_tiles import * # rocks
 
 
 class Level:
     def __init__(self) -> None:
 
         self.display_surface = display.get_surface() # get display surface
-        self.visible_sprintes=sprite.Group()
+        self.visible_sprites=sprite.Group()
         self.col_sprites=sprite.Group()
 
+        self.create_map() # sprite setup below
+
+    def create_map(self):
+        for index_r,row in enumerate(WORLD_MAP):
+            #print(index,row)
+            for index_c,col in enumerate(row):
+                y=index_r * TILESIZE
+                x=index_c * TILESIZE
+                if col=='x':
+                    pos=(x,y)
+                    Tile(pos, [self.visible_sprites])
+                
+                    
+
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface) # draw all sprites in sprite group
+        
--- zelda_style_game_tiles.py
+++ zelda_style_game_tiles.py
@@ -4,5 +4,5 @@ class Tile(sprite.Sprite):
     def __init__(self,pos, groups):
         super().__init__(groups)
 
-        self.image= image.load('../zelda_style_graphics/rock.png').convert_alpha()
+        self.image= image.load('zelda_style_graphics/rock.png').convert_alpha()
         self.rect=self.image.get_rect(topleft=pos)
