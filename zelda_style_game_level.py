635d4650793abeb29027507d1617162174df581b
jamad <jamad2001@gmail.com>
2023-01-05 18:36:29 +0200

    added camera offset

--- zelda_style_game_level.py
+++ zelda_style_game_level.py
@@ -4,7 +4,6 @@ from zelda_style_game_tiles import * # rocks
 from zelda_style_game_player import *
 from zelda_style_game_debug import my_debug # for debug info
 
-
 class Level:
     def __init__(self) -> None:
 
@@ -15,7 +14,9 @@ class Level:
 
         self.col_sprites=sprite.Group()
 
+        self.player=None # placeholder 
         self.create_map() # sprite setup below
+
 
     def create_map(self):
         for index_r,row in enumerate(WORLD_MAP):
@@ -28,11 +29,12 @@ class Level:
                     Tile(pos, [self.visible_sprites,self.col_sprites])
                 if col=='p':
                     pos=(x,y)
+                    
                     self.player = Player(pos, [self.visible_sprites], self.col_sprites)
                 
     def run(self):
         #self.visible_sprites.draw(self.display_surface) # draw all sprites in sprite group
-        self.visible_sprites.custom_draw()
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
 
         my_debug(self.player.direction)#
@@ -42,7 +44,20 @@ class YSortCameraGroup(sprite.Group):
         super().__init__()
 
         self.display_surface = display.get_surface() # get display surface
+        
+        #define camera offset here
+        self.offset=math.Vector2()
 
-    def custom_draw(self):
+    def custom_draw(self, player):
+        
+        half_w,half_h=(x//2 for x in self.display_surface.get_size())
+
+        self.offset.x = -player.rect.centerx + half_w
+        self.offset.y = -player.rect.centery + half_h
+
         for my_sprite in self.sprites():
-            self.display_surface.blit(my_sprite.image, my_sprite.rect)
+            
+            #myrect= (my_sprite.rect.x, my_sprite.rect.y + offset, my_sprite.rect.w, my_sprite.rect.h)
+            myrect=my_sprite.rect.topleft + self.offset  # 2d vector addition
+
+            self.display_surface.blit(my_sprite.image, myrect)
