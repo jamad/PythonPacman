470349fe767493d525f95f393e816233a38204f6
jamad <jamad2001@gmail.com>
2023-01-05 14:10:41 +0200

    Update zelda_style_game.py

--- zelda_style_game.py
+++ zelda_style_game.py
@@ -1,16 +1,50 @@
 from pygame import *
 #from settings import *
 
+#from zelda_style_game_debug import my_debug
+def my_debug(info, y=10, x=10):
+    myfont=font.Font(None,30)
+    display_surface=display.get_surface()
+    debug_surf=myfont.render(str(info), True,'white')
+    debug_rect=debug_surf.get_rect(topleft=(x,y))
+    draw.rect(display_surface, 'black',debug_rect)
+    display_surface.blit(debug_surf, debug_rect)
+
+
 WIDTH=1280
 HEIGHT=720
 FPS=60
 TILESIZE=64
+
+WORLD_MAP = [
+['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
+['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ','p',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ','x','x','x','x','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ','x'],
+['x',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x','x','x',' ',' ',' ','x'],
+['x',' ',' ',' ',' ',' ',' ','x',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ',' ',' ',' ',' ','x','x','x','x','x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ',' ',' ',' ',' ',' ','x','x','x',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ',' ',' ',' ',' ',' ',' ','x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'],
+]
 
 class Game:
     def __init__(self) -> None:
         init()
         self.screen=display.set_mode((WIDTH,HEIGHT))
         self.clock=time.Clock()
+        display.set_caption('ZELDA') # challenge 1
 
 
     def run(self):
@@ -19,6 +53,11 @@ class Game:
                 if ev.type==QUIT:
                     quit()
                     exit()
+
+            self.screen.fill('black')
+            my_debug('hello :) ')
+            display.update()
+            self.clock.tick(FPS)
 
 game=Game()
 game.run()
